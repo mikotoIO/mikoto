fefdc150e9c263a2cffc13bf97b9ac3573e679a3
TheCactusBlue <thecactusblue@gmail.com>
2022-04-28 20:18:34 +0930

    reindent with 2 spaces

--- apps/client/src/constants.ts
+++ apps/client/src/constants.ts
@@ -1,6 +1,6 @@
 const constants = {
-    apiPath: "http://localhost:9500",
-    defaultSpace: "bcc723e1-c8c9-4489-bc58-7172d70190eb",
+  apiPath: "http://localhost:9500",
+  defaultSpace: "bcc723e1-c8c9-4489-bc58-7172d70190eb",
 }
 
 export default constants;
--- apps/server/src/constants.ts
+++ apps/server/src/constants.ts
@@ -1,7 +1,7 @@
 const constants = {
-    apiPort: 9500,
-    getApiPath: () => `http://localhost:${constants.apiPort}`,
-    defaultSpace: "bcc723e1-c8c9-4489-bc58-7172d70190eb",
+  apiPort: 9500,
+  getApiPath: () => `http://localhost:${constants.apiPort}`,
+  defaultSpace: "bcc723e1-c8c9-4489-bc58-7172d70190eb",
 }
 
 export default constants;

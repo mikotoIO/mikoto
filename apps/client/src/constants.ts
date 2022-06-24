6c7b703332ce748600e439f7fa32866cd41667c2
TheCactusBlue <thecactusblue@gmail.com>
2022-06-24 09:34:16 +0930

    build client from docker

--- apps/client/src/constants.ts
+++ apps/client/src/constants.ts
@@ -1,6 +1,8 @@
 const constants = {
-  apiPath: 'http://localhost:9500',
-  defaultSpace: 'bcc723e1-c8c9-4489-bc58-7172d70190eb',
+  apiPath:
+    window.location.host === 'mikoto.io'
+      ? 'https://server.dev.mikoto.io'
+      : 'http://localhost:9500',
 };
 
 export default constants;

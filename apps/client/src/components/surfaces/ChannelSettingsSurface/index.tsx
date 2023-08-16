48fca5897a6ec28e8fea9f1244222fc942f31206
TheCactusBlue <thecactusblue@gmail.com>
2023-08-16 02:29:33 -0700

    fix it slightly

--- apps/client/src/components/surfaces/ChannelSettingsSurface/index.tsx
+++ apps/client/src/components/surfaces/ChannelSettingsSurface/index.tsx
@@ -1,5 +1,31 @@
 import { SettingsView } from '../../../views/SettingsViewTemplate';
+import { BaseSettingsSurface } from '../BaseSettingSurface';
+
+const ACCOUNT_SETTING_CATEGORIES = [
+  { code: 'general', tkey: 'channelSettings.general.title' },
+  { code: 'permissions', tkey: 'channelSettings.permissions.title' },
+];
+
+function General() {
+  return <SettingsView>Channel General</SettingsView>;
+}
+
+function Switch({ nav }: { nav: string }) {
+  // TODO
+  switch (nav) {
+    case 'general':
+      return <General />;
+    default:
+      return null;
+  }
+}
 
 export function ChannelSettingsSurface() {
-  return <SettingsView.Container>lol</SettingsView.Container>;
+  return (
+    <BaseSettingsSurface
+      defaultNav="general"
+      categories={ACCOUNT_SETTING_CATEGORIES}
+      switcher={(nav) => <Switch nav={nav} />}
+    />
+  );
 }

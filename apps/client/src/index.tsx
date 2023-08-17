8913041d7c9dc995241f0150294e85baceea15e4
TheCactusBlue <thecactusblue@gmail.com>
2023-08-17 16:18:49 -0700

    disable verbose errors for development recoil

--- apps/client/src/index.tsx
+++ apps/client/src/index.tsx
@@ -32,12 +32,12 @@ if (SENTRY) {
     // Performance Monitoring
     tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
     // Session Replay
-    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
   });
 }
 
-// RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = !env.DEV;
+RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = !env.DEV;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <RecoilRoot>

6c6de0bdce2afef3af53f2e24193e49629163e99
TheCactusBlue <thecactusblue@gmail.com>
2023-09-20 14:54:02 +0930

    improve the rust auth server

--- apps/auth/src/routes/mod.rs
+++ apps/auth/src/routes/mod.rs
@@ -1,9 +1,10 @@
-use serde::{Deserialize, Serialize};
-
+use pwhash::bcrypt;
 use rocket::serde::json::Json;
+use serde::Serialize;
 
 use crate::{
     context::Ctx,
+    models::{Error, LoginRequest, TokenPair},
     prisma::{email_password, user},
 };
 
@@ -21,23 +22,25 @@ pub fn index() -> Json<AppInfo> {
     })
 }
 
-#[derive(Deserialize)]
-pub struct LoginRequest {
-    email: String,
-    password: String,
-}
-
-#[post("/login", format = "json", data = "<data>")]
-pub async fn login(ctx: &Ctx, data: Json<LoginRequest>) {
-    let data = ctx
+#[post("/login", format = "json", data = "<body>")]
+pub async fn login(ctx: &Ctx, body: Json<LoginRequest>) -> Result<(), Error> {
+    let email_pw = ctx
         .prisma
         .email_password()
-        .find_unique(email_password::email::equals(data.email.clone()))
+        .find_unique(email_password::email::equals(body.email.clone()))
         .with(email_password::user::fetch())
         .exec()
         .await
-        .unwrap()
-        .unwrap();
+        .map_err(|_| Error::DatabaseError)?
+        .ok_or(Error::NotFound)?;
 
-    let u = user::include!({ email_password });
+    let cw = email_pw.user.unwrap();
+
+    // let u = user::include!({ email_password });
+
+    if bcrypt::verify(&body.password, &email_pw.password) {
+        return Err(Error::AuthenticationFailed);
+    }
+
+    Ok(())
 }

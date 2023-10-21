e60ff82a7e0c6785c5a72f9b67420d4e99c84ea4
TheCactusBlue <thecactusblue@gmail.com>
2023-10-21 23:33:23 +1000

    start building emoji adder

--- apps/client/src/components/surfaces/SpaceSettingsSurface/Emojis.tsx
+++ apps/client/src/components/surfaces/SpaceSettingsSurface/Emojis.tsx
@@ -1,5 +1,12 @@
+import { Button } from '@mikoto-io/lucid';
+
 import { SettingsView } from '../../../views/SettingsViewTemplate';
 
 export function EmojiSubsurface() {
-  return <SettingsView>emoji surface goes here</SettingsView>;
+  return (
+    <SettingsView>
+      <Button variant="primary">Add Emoji</Button>
+      emoji surface goes here
+    </SettingsView>
+  );
 }

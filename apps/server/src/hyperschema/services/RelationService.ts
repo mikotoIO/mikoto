4cddc9ec61cd7655d0aefb09fc26fd958055a7a0
TheCactusBlue <thecactusblue@gmail.com>
2023-10-29 16:41:42 +1000

    screw dependabot, it got everything wrong

--- apps/server/src/hyperschema/services/RelationService.ts
+++ apps/server/src/hyperschema/services/RelationService.ts
@@ -1,34 +1,12 @@
 import { h } from '../core';
 import { Relation } from '../models';
 
-interface AlphaBetaIdPair {
-  alphaId: string;
-  betaId: string;
-}
-
-function normalizeRelationship<T extends AlphaBetaIdPair>(
-  userId: string,
-  data: T,
-): {
-  userId: string;
-} & Omit<T, 'alphaId' | 'betaId'> {
-  const { alphaId, betaId, ...rest } = data;
-  const isAlpha = alphaId === userId;
-  return {
-    userId: isAlpha ? betaId : alphaId,
-    ...rest,
-  };
-}
-
 export const RelationService = h.service({
   list: h.fn({}, Relation.array()).do(async ({ state, $p }) => {
     const relationships = await $p.relationship.findMany({
-      where: {
-        OR: [{ alphaId: state.user.id }, { betaId: state.user.id }],
-      },
+      where: { userId: state.user.id },
     });
-    const w = relationships.map((x) => normalizeRelationship(state.user.id, x));
 
-    return relationships.map((x) => normalizeRelationship(state.user.id, x));
+    return relationships;
   }),
 });

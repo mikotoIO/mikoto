b856760c40c2060f925ed725ce126a6bb8b3c030
TheCactusBlue <thecactusblue@gmail.com>
2023-11-11 13:09:31 +1000

    start rewriting the content proxy in rust

---
+++ apps/content-proxy/src/main.rs
@@ -0,0 +1,25 @@
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate serde;
+#[macro_use]
+extern crate serde_json;
+
+pub mod error;
+pub mod functions;
+pub mod routes;
+
+use dotenv::dotenv;
+
+#[launch]
+fn rocket() -> _ {
+    dotenv().ok();
+
+    // dbg!(functions::storage::MINIO.to_string());
+
+    rocket::build()
+        .configure(rocket::Config::figment().merge(("port", 9502)))
+        .mount("/", routes![routes::hello, routes::serve::serve])
+}

67cd3625b6fdfefae847c6733202e9ef80d31fa0
TheCactusBlue <thecactusblue@gmail.com>
2023-12-15 11:37:41 +1000

    use proper package name of io.mikoto.android

--- apps/mobile/android/app/src/main/java/io/mikoto/android/MainActivity.java
+++ apps/mobile/android/app/src/main/java/io/mikoto/android/MainActivity.java
@@ -1,4 +1,4 @@
-package com.mobile;
+package io.mikoto.android;
 
 import android.os.Build;
 import android.os.Bundle;
--- apps/mobile/android/app/src/main/java/io/mikoto/android/MainApplication.java
+++ apps/mobile/android/app/src/main/java/io/mikoto/android/MainApplication.java
@@ -1,4 +1,4 @@
-package com.mobile;
+package io.mikoto.android;
 
 import android.app.Application;
 import android.content.res.Configuration;

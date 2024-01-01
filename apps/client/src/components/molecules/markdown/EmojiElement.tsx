9ac2e3bfe0926dddc7555cd56d3160376db2c0bf
TheCactusBlue <thecactusblue@gmail.com>
2024-01-02 03:51:09 +1000

    add other files

---
+++ apps/client/src/components/molecules/markdown/EmojiElement.tsx
@@ -0,0 +1,24 @@
+import Tippy from '@tippyjs/react';
+import { Tooltip } from '../../atoms/Tooltip';
+import '../EmojiPicker';
+
+export default function Emoji({ emoji }: { emoji: string }) {
+  return (
+    <Tippy
+      animation={false}
+      content={<Tooltip>:{emoji}:</Tooltip>}
+      placement="top"
+      offset={[0, 32]}
+    >
+      {/* @ts-expect-error 2339 */}
+      <em-emoji
+        id={emoji}
+        className="emoji"
+        set="twitter"
+        size="1.2em"
+        fallback={`:${emoji}:`}
+        style={{ verticalAlign: 'middle' }}
+      />
+    </Tippy>
+  );
+}

e1f76bb6d9ebc9fc69cd30e1fd53e198089d8139
TheCactusBlue <thecactusblue@gmail.com>
2024-01-22 09:33:32 +1100

    unbreak the flex model

--- apps/client/src/components/ContextMenu.tsx
+++ apps/client/src/components/ContextMenu.tsx
@@ -26,8 +26,8 @@ const StyledContextMenuOverlay = styled.div`
   pointer-events: none;
   top: 0;
   left: 0;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
 `;
 
 const ContextMenuBase = styled.div`

1f8fb061f62f4586d771149da5a84a3028bfe98d
TheCactusBlue <thecactusblue@gmail.com>
2024-01-23 19:28:32 +1100

    on this side as well

--- apps/desktop/src/preload.ts
+++ apps/desktop/src/preload.ts
@@ -1,2 +1,9 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+import { contextBridge, ipcRenderer } from 'electron';
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  minimize: () => ipcRenderer.send('minimize'),
+  hide: () => ipcRenderer.send('hide'),
+  close: () => ipcRenderer.send('close'),
+});

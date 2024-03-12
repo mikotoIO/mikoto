d2119db3a6b7a486dfff737452b168348b42e2e0
TheCactusBlue <thecactusblue@gmail.com>
2024-03-12 22:38:51 +1000

    upgrade the rust-based imageserver as well

--- apps/content-proxy/src/functions/storage.rs
+++ apps/content-proxy/src/functions/storage.rs
@@ -1,28 +1,35 @@
 use s3::Bucket;
-use url::Url;
 
-use crate::env::MINIO;
+use std::env;
 
 lazy_static! {
     pub static ref MAIN_BUCKET: Bucket = {
-        let minio =
-            Url::parse(&MINIO).unwrap();
+        let protocol = match env::var("S3_USE_SSL") {
+            Ok(x) => if x == "true" { "https" } else { "http" },
+            Err(_) => "http",
+        };
+        let endpoint = env::var("S3_ENDPOINT").expect("S3_ENDPOINT is not provided");
+        let port = match env::var("S3_PORT") {
+            Ok(x) => format!(":{}", x),
+            Err(_) => "".to_owned(),
+        };
+
         let region = s3::Region::Custom {
             region: "".to_owned(),
-            endpoint: minio.origin().unicode_serialization(),
+            endpoint: format!("{}://{}{}", protocol, endpoint, port),
         };
 
         let credentials = s3::creds::Credentials {
-            access_key: Some(minio.username().to_owned()),
-            secret_key: minio.password().map(|s| s.to_owned()),
+            access_key: Some(env::var("S3_ACCESS_KEY").expect("S3_ACCESS_KEY is not provided")),
+            secret_key: env::var("S3_SECRET_KEY").ok(),
             security_token: None,
             session_token: None,
             expiration: None,
         };
 
         // remove the leading slash
-        let bucket_name = minio.path().strip_prefix("/").expect("The URL path must start with a slash");
-        Bucket::new(bucket_name, region.clone(), credentials.clone())
+        let bucket_name = env::var("S3_BUCKET").expect("S3_BUCKET is not provided");
+        Bucket::new(&bucket_name, region.clone(), credentials.clone())
             .expect("Failed to create bucket")
             .with_path_style()
     };
--- apps/content-proxy/src/routes/upload.rs
+++ apps/content-proxy/src/routes/upload.rs
@@ -9,8 +9,9 @@ use crate::{
     error::Error,
     functions::storage::MAIN_BUCKET,
 };
+use image::io::Reader as ImageReader;
 use nanoid::nanoid;
-use rocket::{http::ContentType, serde::json::Json};
+use rocket::serde::json::Json;
 
 #[derive(Debug, Serialize)]
 pub struct UploadResponse {
@@ -23,27 +24,29 @@ pub struct Upload<'r> {
 }
 
 #[post("/<store>", data = "<data>")]
-pub async fn upload(
-    content_type: &ContentType,
-    store: String,
-    data: Form<Upload<'_>>,
-) -> Result<Json<UploadResponse>, Error> {
+pub async fn upload(store: &str, data: Form<Upload<'_>>) -> Result<Json<UploadResponse>, Error> {
     let f = &data.file;
-    let store_config = CONFIG.get(&store).ok_or(Error::NotFound)?;
+    let store_config = CONFIG.get(store).ok_or(Error::NotFound)?;
 
     // validate file size
     if f.len() > store_config.max_size {
-        return Err(Error::BadRequest);
+        return Err(Error::FileTooLarge);
     }
 
     // convert file to buffer
-    let mut f = f.open().await.map_err(|_| Error::BadRequest)?;
-    let buf = f.fill_buf().await.map_err(|_| Error::BadRequest)?;
+    let mut f = f.open().await.map_err(|_| Error::FileBufferError)?;
+    let buf = f.fill_buf().await.map_err(|_| Error::FileBufferError)?;
 
     let (filename, data) = match store_config.store_type {
         StoreType::Image => {
             // validate if image
-            let mut image = image::load_from_memory(buf)?;
+            dbg!(&data.file.len());
+            let mut image = ImageReader::new(Cursor::new(buf))
+                .with_guessed_format()
+                .map_err(|_| Error::ImageError {
+                    internal: "could not guess image format".to_string(),
+                })?
+                .decode()?;
             if let Some(resize) = &store_config.image_resize {
                 // resize image before upload
                 image = image.resize_exact(

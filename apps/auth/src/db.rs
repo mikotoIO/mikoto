48c2843cdb916fc6589f9e84468b2ebb6f7c3dc7
TheCactusBlue <thecactusblue@gmail.com>
2024-08-16 18:09:00 +1000

    share the main db

--- apps/auth/src/db.rs
+++ apps/auth/src/db.rs
@@ -8,21 +8,21 @@ use crate::{env::env, error::Error};
 
 static DB: OnceCell<PgPool> = OnceCell::const_new();
 
-
 pub async fn init() -> Result<&'static PgPool, Error> {
     let db_url = &env().database_url;
     DB.get_or_try_init(|| async {
-        if !Postgres::database_exists(&db_url).await.unwrap() {
-            warn!("Database does not exist, creating...");
-            Postgres::create_database(&db_url).await.unwrap();
-        }
+        // Share the main DB for now
+        // if !Postgres::database_exists(&db_url).await.unwrap() {
+        //     warn!("Database does not exist, creating...");
+        //     Postgres::create_database(&db_url).await.unwrap();
+        // }
 
         let pool = timeout(Duration::from_secs(15), sqlx::PgPool::connect(db_url))
             .await
             .unwrap()
             .unwrap();
 
-        sqlx::migrate!().run(&pool).await.unwrap();
+        // sqlx::migrate!().run(&pool).await.unwrap();
         Ok(pool)
     })
     .await

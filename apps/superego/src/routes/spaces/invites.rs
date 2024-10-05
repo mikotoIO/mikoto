e1ac5b25264329d6a6d2e9503280c3e693034d4b
TheCactusBlue <thecactusblue@gmail.com>
2024-10-05 17:34:34 +1000

    Update the `invites.rs` route file to use the new methods for creating and deleting invites

--- apps/superego/src/routes/spaces/invites.rs
+++ apps/superego/src/routes/spaces/invites.rs
@@ -1,9 +1,11 @@
 use aide::axum::routing::{delete_with, get_with, post_with};
 use axum::{extract::Path, Json};
+use nanoid::nanoid;
 use schemars::JsonSchema;
 use uuid::Uuid;
 
 use crate::{
+    db::db,
     entities::Invite,
     error::Error,
     routes::{router::AppRouter, ws::state::State},
@@ -19,15 +21,23 @@ async fn create(
     _space_id: Path<Uuid>,
     _body: Json<InviteCreatePayload>,
 ) -> Result<Json<Invite>, Error> {
-    Err(Error::Todo)
+    let invite = Invite {
+        id: nanoid!(12),
+        space_id: Uuid::new_v4(),
+        created_at: chrono::Utc::now().naive_utc(),
+        creator_id: Uuid::new_v4(),
+    };
+    invite.create(db()).await?;
+    Ok(invite.into())
 }
 
 async fn list(_space_id: Path<Uuid>) -> Result<Json<Vec<Invite>>, Error> {
     Ok(Json(vec![]))
 }
 
-async fn delete(_id: Path<(Uuid, Uuid)>) -> Result<Json<()>, Error> {
-    Err(Error::Todo)
+async fn delete(Path((_space_id, invite_id)): Path<(Uuid, Uuid)>) -> Result<Json<()>, Error> {
+    Invite::delete(&invite_id.to_string(), db()).await?;
+    Ok(Json(()))
 }
 
 pub fn router() -> AppRouter<State> {

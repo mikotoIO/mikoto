ee08a18978976e77269eb1e4c35799af98d3ff8c
TheCactusBlue <thecactusblue@gmail.com>
2024-10-25 13:25:38 +1000

    clean up everything

--- apps/server/src/auth/index.ts
+++
@@ -1,3 +0,0 @@
-export interface AccountJwt {
-  sub: string;
-}
--- apps/server/src/functions/prismaRecursive.ts
+++
@@ -1,7 +0,0 @@
-import { DMMFClass } from '@prisma/client/runtime';
-
-import { prisma } from './prisma';
-
-// const dmmf = (prisma as any)._baseDmmf as DMMFClass;
-
-// console.log(JSON.stringify(dmmf.datamodel.models, null, 2));

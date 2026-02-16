12ddf7e4a926987c6ac5b47b109f873733590d33
TheCactusBlue <thecactusblue@gmail.com>
2026-02-15 18:07:59 -0800

    more design changes

--- apps/client/src/components/WelcomePanel.tsx
+++ apps/client/src/components/WelcomePanel.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Text } from '@chakra-ui/react';
+import { Center, Heading, Text } from '@chakra-ui/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { faMikoto } from '@/components/icons';
@@ -13,9 +13,7 @@ export function WelcomePanel() {
       bg="surface"
     >
       <FontAwesomeIcon icon={faMikoto} fontSize="10vw" />
-      <Box fontSize="20px" mt="10px" mb="4px">
-        Welcome to Mikoto
-      </Box>
+      <Heading mb="4px">Welcome to Mikoto</Heading>
       <Text color="gray.400" fontSize="14px">
         Open a channel from the sidebar to get started
       </Text>

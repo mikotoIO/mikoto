a0bdaef73ed8c1f2aed23c65c657d281bd187b76
TheCactusBlue <thecactusblue@gmail.com>
2026-04-22 20:10:03 -0700

    Further update to date formatting

--- apps/client/src/components/surfaces/Messages/DateSeparator.tsx
+++ apps/client/src/components/surfaces/Messages/DateSeparator.tsx
@@ -2,7 +2,11 @@ import { Box } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 import { MessageExt } from '@mikoto-io/mikoto.js';
 
-import { MIKOTO_DATE_FORMAT } from './Message/Timestamp';
+export const DATE_FORMAT = new Intl.DateTimeFormat('en', {
+  day: 'numeric',
+  month: 'long',
+  year: 'numeric',
+});
 
 const DAYS_OF_WEEK = [
   'Sunday',
@@ -38,7 +42,7 @@ export function DateSeparator({ date }: { date: Date }) {
   return (
     <StyledDateSeparator>
       <Box bg="gray.750" color="gray.400" px={4} py={0.5} rounded="xl">
-        {DAYS_OF_WEEK[date.getDay()]} {MIKOTO_DATE_FORMAT.format(date)}
+        {DAYS_OF_WEEK[date.getDay()]} {DATE_FORMAT.format(date)}
       </Box>
     </StyledDateSeparator>
   );
--- apps/client/src/components/surfaces/Messages/Message/Timestamp.tsx
+++ apps/client/src/components/surfaces/Messages/Message/Timestamp.tsx
@@ -4,11 +4,18 @@ function padTime(n: number): string {
   return String(n).padStart(2, '0');
 }
 
-export const MIKOTO_DATE_FORMAT = new Intl.DateTimeFormat('en', {
+export const DATE_FORMAT = new Intl.DateTimeFormat('en', {
   day: 'numeric',
   month: 'long',
   year: 'numeric',
 });
+
+function dateFormat(date: Date): string {
+  if (isToday(date)) {
+    return `${padTime(date.getHours())}:${padTime(date.getMinutes())}`;
+  }
+  return `${DATE_FORMAT.format(date)} ${padTime(date.getHours())}:${padTime(date.getMinutes())}`;
+}
 
 function isToday(someDate: Date): boolean {
   const today = new Date();
@@ -22,8 +29,7 @@ function isToday(someDate: Date): boolean {
 export function Timestamp({ time, ...rest }: { time: Date } & BoxProps) {
   return (
     <Box color="gray.400" fontSize="xs" {...rest}>
-      {isToday(time) ? 'Today at ' : MIKOTO_DATE_FORMAT.format(time)}{' '}
-      {padTime(time.getHours())}:{padTime(time.getMinutes())}
+      {dateFormat(time)}
     </Box>
   );
 }

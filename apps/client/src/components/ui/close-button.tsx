2cd03a01a31d35559ab99360df1bd1ca20c82843
Sinclair Chen <abc.sinclair@gmail.com>
2026-06-30 14:43:44 -0700

    remove react-icons

--- apps/client/src/components/ui/close-button.tsx
+++ apps/client/src/components/ui/close-button.tsx
@@ -1,14 +1,15 @@
 import type { ButtonProps as ChakraCloseButtonProps } from '@chakra-ui/react';
 import { IconButton as ChakraIconButton } from '@chakra-ui/react';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import 'react';
-import { LuX } from 'react-icons/lu';
 
 export type CloseButtonProps = ChakraCloseButtonProps;
 
 export const CloseButton = function CloseButton(props: CloseButtonProps) {
   return (
     <ChakraIconButton variant="ghost" aria-label="Close" {...props}>
-      {props.children ?? <LuX />}
+      {props.children ?? <FontAwesomeIcon icon={faXmark} />}
     </ChakraIconButton>
   );
 };
